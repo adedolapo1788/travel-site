e27a07823da85f20394ea7fa25a7fd552efbdf46
Olamide Adedolapo <olamidetunechi@gmail.com>
2018-03-09 16:08:51 +0100

    sticky header done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import RevealOnScroll from "./modules/RevealOnScroll";
 import MobileMenu from "./modules/MobileMenu";
 import $ from "jquery";
+import StickyHeader from "./modules/StickyHeader";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'),"85%");
 new RevealOnScroll($('.testimonial'),"60%");
+var stickyheader = new StickyHeader();

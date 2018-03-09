4a94c81fe09a5bb817bcea64f018a3720ff2ea41
Olamide Adedolapo <olamidetunechi@gmail.com>
2018-03-09 13:03:13 +0100

    done with the reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
-
-
+import RevealOnScroll from "./modules/RevealOnScroll";
 import MobileMenu from "./modules/MobileMenu";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'),"85%");
+new RevealOnScroll($('.testimonial'),"60%");
